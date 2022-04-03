3e721d3fc3516bd963a22185f24d17ae2b8ebf3b
HusseinBarakat89 <68779868+HusseinBarakat89@users.noreply.github.com>
2022-04-03 17:40:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 import torch
-from torchvision import transforms
+import torchvision.transforms as transforms
 import cv2
-import numpy as np
+
 
 class App:
     def __init__(self):
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.face_cascade = cv2.CascadeClassifier('./face_detection/haarcascade_frontalface_default.xml')
         self.model = torch.jit.load('model_scripted.pt')
-        self.model.eval()
-        self.model.to(self.device)
-        self.transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(0., 255.), transforms.Resize((48, 48))])
+        self.emotion_dict = {0: 'Angry', 1: 'Disgust', 2: 'Fear', 3: 'Happy', 4: 'Sad', 5: 'Surprise', 6: 'Neutral'}
+        self.transform = transforms.Compose([transforms.ToTensor()])
 
     def detect(self, img):
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = self.face_cascade.detectMultiScale(gray, 1.1, 4)
+        faces = self.face_cascade.detectMultiScale(img)
         for (x, y, w, h) in faces:
             cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            if w>0 and h>0:
-                img_for_model = np.array(gray[x:x+w, y:y+h])
-                cv2.imshow('img', img_for_model)
-                with torch.no_grad():
-                    y = self.model(self.transform(img_for_model).to(self.device)).argmax(1)
-                    print(y.item())
-        #cv2.imshow('img', img)
+            img_for_model = cv2.resize(gray[y:y + h, x:x + w], (48, 48)) / 255
+            img_for_model = self.transform(img_for_model).unsqueeze(0)
+            with torch.no_grad():
+                self.model.eval()
+                self.model.float()
+                y = self.model.cpu()(img_for_model.float())
+                pred = self.emotion_dict[y.argmax(1).item()]
+                print(pred)
+        cv2.imshow('img', img)
+
+
 
 if __name__ == '__main__':
     app = App()
@@ -32,12 +34,9 @@ if __name__ == '__main__':
         # Read the frame
         _, img = cap.read()
         app.detect(img)
-        
+
         key = cv2.waitKey(30) & 0xff
-        if key==27:
+        if key == 27:
             break
     cap.release()
-
-
-
-
+    cv2.destroyAllWindows()
